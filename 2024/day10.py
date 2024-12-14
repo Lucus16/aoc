712542d50b5edb356d73b64411226e02ad47f4c0
Lars Jellema <lars.jellema@gmail.com>
2024-12-14 12:10:40 +0100

    Simplify day 10

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,16 +1,14 @@
-from collections import defaultdict, Counter
+from collections import Counter
 
 with open("day10.in", "r") as f:
     heights = {x + y * 1j: int(h) for y, row in enumerate(f) for x, h in enumerate(row.strip())}
 
 trailheads = {pos: Counter([pos]) for pos, h in heights.items() if h == 0}
-for _ in range(9):
-    new_trailheads = defaultdict(Counter)
-    for pos, heads in trailheads.items():
-        for step in [1, 1j, -1, -1j]:
-            if heights.get(pos + step) == heights[pos] + 1:
-                new_trailheads[pos + step].update(heads)
-    trailheads = new_trailheads
+for height in range(1, 10):
+    trailheads = {
+        pos: sum((trailheads.get(pos + d, Counter()) for d in [1, 1j, -1, -1j]), start=Counter())
+        for pos, h in heights.items() if h == height
+    }
 
 print(sum(len(heads) for heads in trailheads.values()))
 print(sum(heads.total() for heads in trailheads.values()))
